7a3d14c690827874f88d6e14f13bedede56211ed
liu youshui <llyyss88@163.com>
2019-04-17 16:57:53 +0800

    delete cs gs

--- scripts/delete_cs.ps1
+++ scripts/delete_cs.ps1
@@ -2,11 +2,11 @@ $p=pwd
 echo '---------------------------------------------------'
 echo 'find-------------------------------------------------'
 Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs
-Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs
+Get-ChildItem $p\Protobuf\Generated -recurse *.g.gs
 echo 'delete----------------------------------------------'
 Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs | rm
-Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs | rm
+Get-ChildItem $p\Protobuf\Generated -recurse *.g.gs | rm
 echo 'delete after find------------------  ---------------'
 Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs
-Get-ChildItem $p\Protobuf\Generated -recurse *.g.cs
+Get-ChildItem $p\Protobuf\Generated -recurse *.g.gs
 echo '---------------------------------------------------'
