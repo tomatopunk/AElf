d3407680a78e95951acc70ef85cb7ce44f891329
liuyoushui <liuyoushui@newbeescm.com>
2019-04-17 20:34:37 +0800

    delete cs gs

--- scripts/delete_cs.sh
+++ scripts/delete_cs.sh
@@ -2,8 +2,8 @@
 p=`pwd`
 echo '--------------------------------------------------------------------'
 echo 'find.............'
-rm -rf  $p/Protobuf/Generated
-rm -rf  $p/Protobuf/Generated
+rm -rf  $p/Protobuf/Generated/*.g.cs
+rm -rf  $p/Protobuf/Generated/*.g.gs
 echo $?
 echo '---------------------------------------------------------------------'
 
